d53178fb467e91c90520a4fd7ae05d2d4f4ae5c6
Swar2424 <154347168+Swar2424@users.noreply.github.com>
2024-03-20 10:59:16 +0100

    remote database

--- README.txt
+++ README.txt
@@ -1,2 +1,4 @@
 SFR GIT :
 https://github.com/sfrenot/web
+
+'C:\Program Files\MongoDB\Server\7.0\bin\mongod.exe' --port 3010 --dbpath ./data
--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,9 @@ const router = express.Router();
 
 // set our port to either a predetermined port number if you have set it up, or 3001
 const API_PORT = process.env.API_PORT || 3001;
-
-
+mongoose.connect("mongodb+srv://Admin:Golmong@web-db.kspqmse.mongodb.net/?retryWrites=true&w=majority&appName=Web-DB");
+var db = mongoose.connection;
+db.on('error', () => console.error('Erreur de connexion'));
 
 // now we should configure the API to use bodyParser and look for JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,6 +23,33 @@ app.use(bodyParser.json());
 // now we can set the route path & initialize the API
 router.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
+});
+router.get('/comments', (req, res) => {
+  Comment.find()
+    .then(comments => {
+      res.json({ success: true, data: comments });
+    })
+    .catch(err => {
+      res.json({ success: false, data: { error: err } });
+    });
+});
+router.post('/comments', (req, res) => {
+  const comment = new Comment();
+  // body parser lets us use the req.body
+  const { author, text } = req.body;
+  if (!author || !text) {
+    // we should throw an error. we can do this check on the front end
+    return res.json({
+      success: false,
+      error: 'You must provide an author and comment'
+    });
+  }
+  comment.author = author;
+  comment.text = text;
+  comment.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
 });
 
 
